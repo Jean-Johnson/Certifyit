f5fc9a376e5b20e7555d82285289e35f5e3390e1
Jean-Johnson <jeanjohnsonldrs@gmail.com>
2019-09-19 00:18:12 +0530

    initial commit

--- Certifyit.py
+++ Certifyit.py
@@ -1,11 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
-image = Image.open('test.jpg')
-draw = ImageDraw.Draw(image)
+x=-1
+y=-1
+def test():
+	loop=''
+	print("[+]Press 'q' to quit testing")
+	while loop!='q':
+		global x,y
+		x,y=map(int, input().split())
+		image = Image.open('certificate.jpg')
+		draw = ImageDraw.Draw(image)
 #font = ImageFont.truetype('Roboto-Bold.ttf', size=45)
-(x,y) = (50,50)
-msg = "Jean"
-color='rgb(0,0,0)'
-
-draw.text((x,y),msg,fill=color)
-image.save('save.jpg')
-image.show()
+		msg = "Jean"
+		color='rgb(0,0,0)'
+		draw.text((x,y),msg,fill=color)
+		image.save('save.jpg')
+		image.show()
+		loop=input("[+]Enter q to quit")
+if __name__=="__main__":
+	while True:
+		cho=int(input("1) Test image to get dimensions\n2)Add names\n"))
+		if cho==1:
+			test()
+		else:
+			print("under development")
+			break
